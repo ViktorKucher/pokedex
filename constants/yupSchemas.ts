469f72ad63a164ed108b268ec8074a88563edd70
Kucher Viktor <viktorkucer49@gmail.com>
2024-01-06 19:27:15 +0200

    Created(configs,constants):configs consist of auth settings

---
+++ constants/yupSchemas.ts
@@ -0,0 +1,28 @@
+import * as Yup from "yup";
+
+const username = Yup.string()
+  .min(6, "Username must be 6 characters long")
+  .required("Username is required");
+
+const email = Yup.string()
+  .email("Invalid email format")
+  .required("Mail is required");
+
+const password = Yup.string()
+  .min(8, "Password must be 8 characters long")
+  .matches(/[0-9]/, "Password requires a number")
+  .matches(/[a-z]/, "Password requires a lowercase letter")
+  .matches(/[A-Z]/, "Password requires an uppercase letter")
+  .matches(/[^\w]/, "Password requires a symbol")
+  .required("Password is required");
+
+export const validationLoginSchema = Yup.object({
+  username,
+  password,
+});
+
+export const validationRegistrationSchema = Yup.object({
+  username,
+  email,
+  password,
+});
