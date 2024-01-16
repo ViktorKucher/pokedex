9f2d2eff668d1a7b9fc17b659ed575a3cc6880ec
Kucher Viktor <viktorkucer49@gmail.com>
2024-01-16 16:09:24 +0200

    deleted(buttons and icons)

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FacebookIconSVG, GoogleIconSVG, IconClose } from "./Icons";
+import { FacebookIconSVG, GoogleIconSVG} from "./Icons";
 interface IButton {
   children: React.ReactNode;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
@@ -60,13 +60,3 @@ export const GoogleButton = ({
   );
 };
 
-export const ButtonClose = () => (
-  <button
-    type="button"
-    className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-    data-dismiss-target="#toast-danger"
-    aria-label="Close"
-  >
-    <IconClose />
-  </button>
-);
--- components/ui/Icons.tsx
+++ components/ui/Icons.tsx
@@ -1,33 +1,3 @@
-export const ErrorIcon = () => (
-  <svg
-    className="w-5 h-5"
-    aria-hidden="true"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="currentColor"
-    viewBox="0 0 20 20"
-  >
-    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
-  </svg>
-);
-
-export const IconClose = () => (
-  <svg
-    className="w-3 h-3"
-    aria-hidden="true"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 14 14"
-  >
-    <path
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-    />
-  </svg>
-);
-
 export const FacebookIconSVG = () => (
   <svg
     className="w-4 h-4 me-2"
