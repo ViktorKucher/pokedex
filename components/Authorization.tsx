dac629f7e1971f48bf6943c69f4f0e998624d9cf
Kucher Viktor <viktorkucer49@gmail.com>
2024-01-17 17:37:30 +0200

    add loader

--- components/Authorization.tsx
+++ components/Authorization.tsx
@@ -3,9 +3,13 @@ import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { Button } from "./ui/Button";
 import { Login } from "./Login";
+import { Loader } from "./ui/Loader";
 
 export const Authorization = () => {
   const {data,status} = useSession();
+  if(status==='loading'){
+    return <Loader typeLoading="fullPage"/>
+  }
   return (
     <div className="flex flex-col m-0 gap-2 w-full max-w-64 items-center p-0 h-screen justify-center">
       {status==='authenticated' ? (
